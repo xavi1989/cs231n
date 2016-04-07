44084bbbfd673cec8d2e0a2c710880a2ba6111e5
Wei Xia <wxia@nvidia.com>
2016-04-06 18:16:50 -0700

    A1: add softmax related code

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,23 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  for j in xrange(X.shape[0]):
+    score = X[j, :].dot(W)
+    score = np.exp(score)
+    nor = np.sum(score)
+    score = score / nor
+    loss += (-np.log(score[y[j]]))
+
+    for i in xrange(W.shape[1]):
+        dW[:, i] += (score[i] - (i == y[j])) * X[j, :]
+
+  loss = loss / X.shape[0]
+  reg_loss = 0.5 * reg * np.sum(W * W)
+  loss = loss + reg_loss
+
+  dW = dW / X.shape[0]
+  dW = dW + reg * W
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -46,14 +62,34 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+  num_train = X.shape[0]
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  score = X.dot(W)
+  score = np.exp(score)
+  nor = np.sum(score, axis = 1)
+  score = score.T / nor
+  #score C * D
+  m_loss = np.choose(y, score)
+  loss = -np.sum(np.log(m_loss))
+  loss = loss / num_train
+  reg_loss = 0.5 * reg * np.sum(W * W)
+  loss = loss + reg_loss
+
+  # dW
+  dW1 = X.T.dot(score.T)
+  mask = np.zeros_like(score)
+  mask[y, xrange(num_train)] = 1
+  dW2 = X.T.dot(mask.T)
+
+  dW = dW1 - dW2
+  dW = dW / num_train
+  reg_dW = reg * W
+  dW = dW + reg_dW
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
