4d733b51d372bcaa58c7445a0286fa80af73078f
Wei Xia <wxia@nvidia.com>
2016-05-26 13:59:08 -0700

    stats315b: add more about hw3 script

--- stats315b/hw3/hw3_script.R
+++ stats315b/hw3/hw3_script.R
@@ -1,13 +1,13 @@
 library(nnet)
-dir = "/home/wxia/wei_project/cs231n/stats315b/"
+dir = "C:/Users/wxia/Desktop/learning-stuff/312B-2016Spring/data"
 
 setwd(dir)
 rm(list = ls())
 
-spam_predictors =  read.csv("Marketing/Spam_Names.txt",skip = 33, sep =":",strip.white = TRUE, header=FALSE, colClasses = "character")
+spam_predictors =  read.csv("Spam/Spam_Names.txt",skip = 33, sep =":",strip.white = TRUE, header=FALSE, colClasses = "character")
 spam_names = c(spam_predictors[,1],'type')
 
-spam_df = read.csv("Marketing/Spam_Data.txt", header = FALSE)
+spam_df = read.csv("Spam/Spam_Data.txt", header = FALSE)
 colnames(spam_df) = spam_names
 
 #data pre-process
@@ -25,7 +25,7 @@ spam_test = spam_df_processed[testIndex,]
 spam_train = spam_df_processed[-testIndex, ]
 
 nHidden = 10
-nDiffsets = 5
+nDiffsets = 15
 nnRate = array(dim = c(nHidden, nDiffsets))
 maxRate = 0
 
@@ -38,17 +38,14 @@ for (i in seq(1, nHidden, by = 1)) {
     test_predict = predict(nn, spam_test, type = 'class')
     
     right = sum(test_predict == (spam_test$type)[, 2])
-    # right classification rate
-    rate = right / nrow(spam_test)
+    # misclassification rate
+    rate = 1 - right / nrow(spam_test)
     nnRate[i, j] = rate
-    if(rate > maxRate) {
-      maxRate = rate
-      bestModel = nn
-    }
   }
 }
 
-bestHidden = which.max(rowMeans(nnRate))
+plot(1:nHidden, rowMeans(nnRate), type = 'l')
+bestHidden = which.min(rowMeans(nnRate))
 
 wdLength = 10
 wdRate  = array(dim = c(wdLength, 1))
@@ -61,12 +58,12 @@ for (wd in seq(0, 1, length.out = wdLength)) {
   
   right = sum(test_predict == (spam_test$type)[, 2])
   # right classification rate
-  rate = right / nrow(spam_test)
+  rate = 1 - right / nrow(spam_test)
   wdRate[i] = rate
   i = i + 1
 }
 
-index = which.max(wdRate)
+index = which.min(wdRate)
 #weight decay fine tuning
 wdLength = 100
 wdfineRate = array(dim = c(wdLength, 1))
@@ -79,7 +76,21 @@ for(wd in seq((index-1)*0.1, (index+1)*0.1, length.out = wdLength)) {
   
   right = sum(test_predict == (spam_test$type)[, 2])
   # right classification rate
-  rate = right / nrow(spam_test)
-  wdRate[i] = rate
+  rate = 1 - right / nrow(spam_test)
+  wdfineRate[i] = rate
   i = i + 1  
 }
+index2 = which.min(wdfineRate)
+wdfinetuning = (index-1) * 0.1 + 0.2 * index2 / 100
+min(wdfineRate)
+
+#(c)
+nn = nnet(type~. , data = spam_train, size = bestHidden, softmax = TRUE,
+          maxit = 1000, rang = 0.5, decay = wdfinetuning, trace = F)
+
+test_predict = predict(nn, spam_test, type = 'raw')
+spamIndex = test_predict[, 2] > 0.98
+test_01 = seq(0, 0, length.out = nrow(spam_test))
+test_01[spamIndex] = 1
+
+t = table(spam_test$type[, 2], test_01)
