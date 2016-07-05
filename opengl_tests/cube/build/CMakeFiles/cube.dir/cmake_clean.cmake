33aa610ce2957d1f45d133b1879ada43dfbe5b4d
Wei Xia <wxia@nvidia.com>
2016-07-05 16:18:30 -0700

    add open gl sample code

---
+++ opengl_tests/cube/build/CMakeFiles/cube.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cube.dir/cube.c.o"
+  "cube.pdb"
+  "cube"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/cube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ opengl_tests/dinoshade/build/CMakeFiles/dinoshade.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/dinoshade.dir/dinoshade.c.o"
+  "dinoshade.pdb"
+  "dinoshade"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/dinoshade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
