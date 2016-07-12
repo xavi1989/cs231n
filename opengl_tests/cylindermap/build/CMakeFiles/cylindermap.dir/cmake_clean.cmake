598e2a72bec1a6c0c233945ed1fdd31a4a74e2ed
Wei Xia <wxia@nvidia.com>
2016-07-12 16:50:58 -0700

    build cylinder map code

---
+++ opengl_tests/cylindermap/build/CMakeFiles/cylindermap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cylindermap.dir/cylindermap.cpp.o"
+  "cylindermap.pdb"
+  "cylindermap"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cylindermap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
